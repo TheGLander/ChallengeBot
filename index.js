b2b22356ba5922b8872a9f5ab547e1f6158b55ad
Lubomir <lubomir.yudenko@gmail.com>
2019-06-27 09:54:04 +0300

    Yay challenges

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table'
 if (!table['count(*)']) {
     sql.prepare("CREATE TABLE users (id TEXT PRIMARY KEY, user TEXT, guild TEXT, coins INTEGER, badges TEXT, other TEXT);").run();
     sql.prepare("CREATE UNIQUE INDEX idx_users_id ON users (id);").run();
-    sql.prepare("CREATE TABLE servers (id TEXT PRIMARY KEY, guild TEXT channel TEXT, other TEXT);").run();
+    sql.prepare("CREATE TABLE servers (id TEXT PRIMARY KEY, channel TEXT, other TEXT);").run();
     sql.prepare("CREATE UNIQUE INDEX idx_servers_id ON servers (id);").run();
     sql.pragma("synchronous = 1");
     sql.pragma("journal_mode = wal");
@@ -23,8 +23,19 @@ getData = sql.prepare("SELECT * FROM users WHERE user = ? AND guild = ?");
 setData = sql.prepare("INSERT OR REPLACE INTO users (id, user, guild, coins, badges, other) VALUES (@id, @user, @guild, @coins, @badges, @other);");
 //Importing some useful functions
 const useful = require('gsusefulfunctions')
-/*      # #                     # #        
-       # #     # # # # # # #     # #      
+//List of current challenges
+var challengelist = {
+    "template": {
+        "type": 0,
+        "letter": "g",
+        "words": [
+            "guilty",
+            "gorgeous"
+        ]
+    }
+}
+/*         
+          # # # # # # # # # # # #      
      # # # # # # # # # # # # # # # # #    
      # # # # # # # # # # # # # # # # #    
      # # # # # # # # # # # # # # # # #    
@@ -40,12 +51,35 @@ const useful = require('gsusefulfunctions')
      # # # #                   # # # #    
        # # # #               # # # # */
 
-function NewChallenge(guildID) {
+function NewChallenge(guildID, type) {
     //Wait for 4-6 minutes
-    setTimeout(function () {
-        const Data = sql.prepare(`SELECT * FROM servers WHERE guild = ?`).get(guildID.toString())
-        const channel = client.channels.get(Data.channel);
-    }, ( /*240000 + Math.round(120000 * Math.floor())*/ 0))
+    const timeout = 0//240000 + Math.round(120000 * Math.random())
+    setTimeout(function (a) {
+        const Data = sql.prepare(`SELECT * FROM servers WHERE id = ?`).get(guildID)
+        if (Data === undefined) {
+            //
+            return;
+        }
+        switch (type) {
+            case 0:
+                const channel = client.channels.get(Data.channel);
+                const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+                const defwords = ["American", "Born", "Confused", "Desk", "Emigrated", "From", "Gujarat", "House", "In", "Jersey", "Kids", "Learning", "Medicine", "Now", "Owning", "Property", "Quite", "Reasonable", "Salary", "Two", "Ukraine", "Visiting", "White", "Xenophobic", "Yet", "Zestful"]
+                const letterID = Math.floor(Math.random() * letters.length)
+                const letter = letters[letterID]
+                challengelist[guildID] = {
+                    "type": 0,
+                    "letter": letter,
+                    "words": [defwords[letterID].toLowerCase()]
+                }
+                channel.send(`⭐                                        Challenge!                                        ⭐\nWrite as many words as you can which start with the letter **${letter.toUpperCase()}**!\n                                            Example: ${defwords[letterID]}\n                  For every right word you will gain a coin!\n                                    You have **2 minutes**!`);
+                setTimeout(function () {
+                    channel.send("Time's up!")
+                    delete challengelist[guildID]
+                }, config.QuizTimeLimit)
+                break;
+        }
+    }, timeout)
 }
 //The commands the bot can execute
 const Commands = {
@@ -66,11 +100,14 @@ const Commands = {
             }
             var row = {
                 "id": msg.guild.id,
-                "guild": msg.guild.id,
                 "channel": client.channels.get(collected.content.replace(/[<>#]/g, "")).id,
                 "other": ""
             }
-            sql.prepare("INSERT OR REPLACE INTO servers (id, guild, channel, other) VALUES (@id, @guild, @channel, @other);").run(row)
+            try {
+                sql.prepare("INSERT OR REPLACE INTO servers (id, channel, other) VALUES (@id, @channel, @other);").run(row)
+            } catch (error) {
+                console.log(error)
+            }
             msg.channel.send("Setup done!")
         });
         collector.on('end', end => {
@@ -117,7 +154,27 @@ client.on('message', (msg) => {
     if (msg.author.bot) {
         return;
     };
-    if (useful.in_array(msg.content.substr(3).split(" ")[0], Object.keys(Commands)) && msg.content.substr(0, 3) == config.prefix) {
-        Commands[msg.content.substr(3).split(" ")[0]](msg)
+    if (useful.in_array(msg.content.substr(config.prefix.length).split(" ")[0], Object.keys(Commands)) && msg.content.substr(0, config.prefix.length) == config.prefix) {
+        Commands[msg.content.substr(config.prefix.length).split(" ")[0]](msg)
+    } else if (challengelist[msg.guild.id] !== undefined) {
+        var challenge = challengelist[msg.guild.id]
+        switch (challenge.type) {
+            case 0:
+                if (!(msg.content.toLowerCase().startsWith(challenge.letter))) {
+                    msg.channel.send(`Word doesn't start with **${challenge.letter.toUpperCase()}**!`)
+                    return;
+                }
+                if (!dictionary.check(msg.content)) {
+                    msg.channel.send("Invalid word!")
+                    return;
+                }
+                if (useful.in_array(msg.content.toLowerCase(), challenge.words)) {
+                    msg.channel.send("Word already used!")
+                    return;
+                }
+                msg.channel.send(`<@${msg.author.id}> Just submitted a valid word, ${msg.content}!`)
+                challengelist[msg.guild.id].words[challengelist[msg.guild.id].words.length] = msg.content.toLowerCase();
+                break;
+        }
     }
 });

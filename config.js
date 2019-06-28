820613c7de93b9718d4d6a2649602e346120fe72
TheGLander <34986581+TheGLander@users.noreply.github.com>
2019-06-28 18:57:25 +0300

    Because .gitignore

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "token": "NTkzMzcxMTY2MTQwMjY4NTY1.XRM5_g.J7_A_buMAnv_K9WSO41cdu4To5k", //Insert your token here
-    //Don't edit the data below please
-    "prefix":"cb!",
-    "QuizTimeLimit":120000,
-    "debug":false
-}
